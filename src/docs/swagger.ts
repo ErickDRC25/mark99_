1175f70eced8634abf1d145e286b21f2b3242669
Erick Romero <romerocruzerick11@gmail.com>
2025-07-11 21:46:54 -0500

    Revert "Orden swagger"
    This reverts commit 6d0ee6f4e9bef94998d8d2f7e23df44342774e36.

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,37 +1,28 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'API Mark99',
-    version: '1.0.0',
-    description: 'API documentation for Mark99Re',
-  },
-  components: {
-    securitySchemes: {
-      bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
+    openapi: '3.0.0',
+    info: {
+        title: 'API Mark99',
+        version: '1.0.0',
+        description: 'API documentation for Mark99Re',
     },
-  },
-  security: [{ bearerAuth: [] }],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+            },
+        },
+    },
+    security: [{ bearerAuth: [] }],
 
-  tags: [
-    { name: 'Auth', description: 'Autenticación y autorización' },
-    { name: 'Clientes', description: 'Endpoints para gestionar clientes' },
-    { name: 'Proveedores', description: 'Endpoints para gestionar proveedores' },
-    { name: 'Productos', description: 'Endpoints para gestionar productos' },
-    { name: 'Inventario', description: 'Endpoints para gestionar inventario' },
-    { name: 'Ventas', description: 'Endpoints para gestionar ventas' },
-    { name: 'DetalleVenta', description: 'Endpoints para gestionar detalles de venta' },
-  ],
 };
 
 const options = {
     swaggerDefinition,
-    apis: ['./dist/routes/.js', './dist/controllers/.js'],
+    apis: ['./dist/routes/*.js', './dist/controllers/*.js'],
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
